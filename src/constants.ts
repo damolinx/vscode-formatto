84b9cffe5336e4ed510bd51ac0b4cb66cbc21f8e
Daniel Molina <damolina@outlook.com>
2026-04-04 13:35:09 -0700

    Clea-up unused constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,1 @@
-import * as vscode from 'vscode';
-
 export const EXTENSION_PREFIX = 'formatto';
-
-export const PACKAGE_FILENAME = '__package.rb';
-
-export const DOCUMENT_SELECTOR: Readonly<vscode.DocumentFilter> = {
-  language: 'ruby',
-  scheme: 'file',
-};
